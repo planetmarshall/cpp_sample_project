bf4f0d0eee56c8124beecd8aa4dc7c2e3395ca77
Andrew Marshall <planetmarshalluk@gmail.com>
2021-08-08 18:32:06 +0100

    [test] add unit tests

--- test/core/test_problem_1.cpp
+++ test/core/test_problem_1.cpp
@@ -1,6 +1,13 @@
-#include <gtest/gtest.h>
-#include <sample/version.hpp>
+#include <euler/euler.h>
 
-TEST(TestVersion, VersionIsNotNull) {
-    ASSERT_EQ("1.0.0", sample::version());
+#include <gtest/gtest.h>
+
+TEST(TestProblem1, ListOfMultiplesBelow10) {
+  auto multiples = euler::multiples_of_three_or_five(10);
+  ASSERT_EQ((std::vector<int>{3, 5, 6, 9}), multiples);
+}
+
+TEST(TestProblem1, SumOfMultiplesBelow10) {
+  auto sum = euler::sum_multiples_of_three_or_five(10);
+  ASSERT_EQ(23, sum);
 }
