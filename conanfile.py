04af44e3d5fd3e06a47df148b61f039cff1fa028
Andrew Marshall <planetmarshalluk@gmail.com>
2021-08-08 18:53:58 +0100

    [ci] use conan to build on ci

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,27 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 
 class CppSampleProjectConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
    generators = "cmake", "cmake_find_package"
+   options = {
+      "fPIC": [True, False],
+      "shared": [True, False]
+   }
+   default_options = {
+      "fPIC": True,
+      "shared": False
+   }
+
+   def config_options(self):
+      if self.settings.os == "Windows":
+         del self.options.fPIC
 
    def build_requirements(self):
       self.build_requires("gtest/1.11.0")
+      self.build_requires("ninja/1.10.2")
+
+   def build(self):
+      cmake = CMake(self, generator="Ninja")
+      cmake.configure()
+      cmake.build()
