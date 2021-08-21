80e10a324917a78d9af90b0678fa34489dd7351b
Andrew Marshall <planetmarshalluk@gmail.com>
2021-08-21 12:48:28 +0100

    [euler] implement problem 1

--- src/euler/problem_1.cpp
+++ src/euler/problem_1.cpp
@@ -1,9 +1,18 @@
 #include <euler/euler.h>
 
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/filter.hpp>
+#include <range/v3/to_container.hpp>
+#include <range/v3/numeric/accumulate.hpp>
+
+namespace views = ranges::views;
+
 std::vector<int> euler::multiples_of_three_or_five(int max) {
-  return {};
+  return  views::ints(3, max)
+          | views::filter([] (int i) { return (i % 3 == 0) || (i % 5 == 0); })
+          | ranges::to<std::vector>();
 }
 
 int euler::sum_multiples_of_three_or_five(int max) {
-  return 0;
+  return ranges::accumulate(euler::multiples_of_three_or_five(max), 0);
 }
