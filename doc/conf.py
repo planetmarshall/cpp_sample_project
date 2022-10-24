cd4bf4b23a5710d975e294318b830778bfdba566
Andrew Marshall <planetmarshalluk@gmail.com>
2022-10-24 16:35:56 +0100

    flesh out the generated docs

--- doc/conf.py
+++ doc/conf.py
@@ -6,13 +6,14 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'cpp_sample_project'
+project = 'C++ Sample Project'
 copyright = '2022, Andrew Marshall'
 author = 'Andrew Marshall'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 
 extensions = []
 
@@ -24,5 +25,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
