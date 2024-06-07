a8f588eb5247dd6ac2d21a66db6ac29e12824da3
Andrew Marshall <planetmarshalluk@gmail.com>
2024-06-07 15:10:16 +0100

    port to conan2 and drop ranges-v3 in favour of STL

--- src/euler/problem_1.cpp
+++ src/euler/problem_1.cpp
@@ -1,20 +1,23 @@
 #include <euler/euler.hpp>
 
-#include <range/v3/numeric/accumulate.hpp>
-#include <range/v3/range/conversion.hpp>
-#include <range/v3/view/filter.hpp>
-#include <range/v3/view/iota.hpp>
+#include <algorithm>
+#include <numeric>
+#include <ranges>
 
-namespace views = ranges::views;
+namespace views = std::ranges::views;
 
 std::vector<int> euler::multiples_of_three_or_five(int max) {
     // NOLINTBEGIN(*-magic-numbers)
-    return views::ints(3, max)
-           | views::filter([](int num) { return (num % 3 == 0) || (num % 5 == 0); })
-           | ranges::to<std::vector>();
+    auto multiples = std::ranges::views::iota(3, max)
+           | views::filter([](int num) { return (num % 3 == 0) || (num % 5 == 0); });
     // NOLINTEND(*-magic-numbers)
+
+    auto result = std::vector<int>{};
+    std::ranges::copy(multiples, std::back_inserter(result));
+    return result;
 }
 
 int euler::sum_multiples_of_three_or_five(int max) {
-    return ranges::accumulate(euler::multiples_of_three_or_five(max), 0);
+    auto multiples = multiples_of_three_or_five(max);
+    return std::accumulate(std::begin(multiples), std::end(multiples), 0);
 }
